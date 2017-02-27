19f160c3881de436e62380bbc25d9322a92275e9
nicolalandro <nicolaxx94@live.it>
2017-02-27 20:54:33 +0100

    combo box

--- GUI.py
+++ GUI.py
@@ -10,14 +10,14 @@ class MainWindow(QtGui.QMainWindow):
     def flash_firmware(self):
         erase1 = "sudo python esptool.py --port "
         erase2 = " --baud 115200 erase_flash"
-        erase = erase1 + str(self.portTextEdit.toPlainText()) + erase2
+        erase = erase1 + str(self.portComboBox.currentText()) + erase2
         # print(erase)
         os.system(erase)
 
         flash1 = "sudo python esptool.py --port"
         flash2 = " write_flash -fm dio -fs 32m 0x00000 "
-        flash = flash1 + str(self.portTextEdit.toPlainText()) + flash2 + str(self.pathTextEdit.toPlainText())
-        #print (flash)
+        flash = flash1 + str(self.portComboBox.currentText()) + flash2 + str(self.pathTextEdit.toPlainText())
+        # print (flash)
         os.system(flash)
 
     def select_firmware(self):
@@ -46,13 +46,12 @@ class MainWindow(QtGui.QMainWindow):
         self.pathTextEdit = QtGui.QTextEdit(cWidget)
         self.pathTextEdit.setMaximumSize(500, 30)
 
-        label = QtGui.QLabel("Write Shield Port")
-        self.portTextEdit = QtGui.QTextEdit(cWidget)
-        self.portTextEdit.setMaximumSize(500, 30)
-        self.portTextEdit.setText("/dev/ttyUSB0")
+        label = QtGui.QLabel("Select shield connected Port")
+        self.portComboBox = QtGui.QComboBox()
+        self.portComboBox.addItem("/dev/ttyUSB0")
 
         grid.addWidget(label, 0, 0)
-        grid.addWidget(self.portTextEdit, 0, 1)
+        grid.addWidget(self.portComboBox, 0, 1, 1, 2)
         grid.addWidget(self.pathTextEdit, 1, 0, 1, 2)
         grid.addLayout(vBoxSelectFirmware, 1, 2)
         grid.addLayout(vBoxFlashing, 2, 0, 3, 0)
